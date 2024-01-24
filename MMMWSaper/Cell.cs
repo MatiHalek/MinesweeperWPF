ee5c457cb52b9a3df6d3d8dd81554c41c7d88cc7
Mateusz Marmuźniak <mateusz.marmuzniak.poland@gmail.com>
2024-01-24 10:12:29 +0100

    Add Cell class

---
+++ MMMWSaper/Cell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMMWSaper
+{
+    public class Cell
+    {
+        public uint PositionX { get; set; }
+        public uint PositionY { get; set; }
+        public bool IsRevealed { get; set; }
+        public bool IsFlagged { get; set; }
+        public bool IsBomb { get; set; }
+
+        public Cell(uint positionX, uint positionY, bool isRevealed, bool isFlagged, bool isBomb)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+            IsRevealed = isRevealed;
+            IsFlagged = isFlagged;
+            IsBomb = isBomb;
+        }
+    }
+}
